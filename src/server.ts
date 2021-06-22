8c1f76ddd4cc4469653823c275750178a727aff2
Jornay <lucasdeafg@yahoo.com.br>
2021-06-21 22:25:53 -0300

    Second Class unfineshed!

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,9 @@
 import express from "express";
+import "reflect-metadata";
 
+import "./database";
 
 const app = express();
 
-//Rotas
-app.get("/teste", (request, response) =>{
-    return response.send("Olá testers do BRASIL")
-})
-
-app.post("/test-post", (request, response) => {
-    return response.send("Olá testers do BRAZIL que utilizam post")
-})
-//====
 
 app.listen(3000, () => console.log("server is running"));
