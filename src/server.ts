cf2149709df33b64558c8ae7b524a63b5b369c58
Jornay <lucasdeafg@yahoo.com.br>
2021-06-22 22:21:20 -0300

    Third Class Unfinished!

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,29 @@
-import express from "express";
+import express, { NextFunction  , Request, Response} from "express";
 import "reflect-metadata";
+import "express-async-errors";
+
+import {router} from "./routes";
 
 import "./database";
 
 const app = express();
 
 
-app.listen(3000, () => console.log("server is running"));
+app.use(express.json()); //LER DADOS NO FORMATO JSON
+
+app.use(router)//INSERE AS ROTAS DO routes.ts
+
+app.use((err : Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error){
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+    
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+})
+
+app.listen(3000, () => console.log("server is running")); 
